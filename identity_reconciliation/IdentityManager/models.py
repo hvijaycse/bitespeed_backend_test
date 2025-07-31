1e0a43506a575fcc86946373d2a38031ca0763c8
Harsh Vijay <hvijay.cse@gmail.com>
2025-07-31 22:45:32 +0530

    MODELS
    - Created model
    - Created migrations for those models
    - updated gitignore to avoid pycache

--- identity_reconciliation/IdentityManager/models.py
+++ identity_reconciliation/IdentityManager/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+class KLinkPrecedence(models.TextChoices):
+
+    PRIMARY = "primary", "primary"
+    SECONDARY = "secondary", "secondary"
+
+
+class Identities(models.Model):
+
+    class Meta:
+        db_table = "bite_speed_indentities"
+    
+    phoneNumber = models.CharField(max_length=10, null=True)
+    email = models.EmailField(null=True)               
+    linkedId = models.OneToOneField(
+        "self",
+        null=True,      
+        on_delete=models.SET_NULL,
+        blank=True,
+        related_name='linked_by'
+    )
+    linkPrecedence = models. CharField(choices=KLinkPrecedence.choices, max_length=10)
+    createdAt = models.DateTimeField(auto_now=True)
+    updatedAt= models.DateTimeField(auto_now_add=True)              
+    deletedAt=models.DateTimeField(null=True)
